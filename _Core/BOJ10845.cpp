0b8a6a1ec77438545f9553b05291725d5122f19a
KLMIL <kanoline@naver.com>
2025-06-11 08:57:38 +0900

    feat(백준): BOJ10845, BOJ11866 - 큐

---
+++ _Core/BOJ10845.cpp
@@ -0,0 +1,98 @@
+﻿/*
+	Site		: BaekJoon Online Judge - https://www.acmicpc.net/
+	Problem		: BOJ 10845
+	URL			: https://www.acmicpc.net/problem/10845
+	Date		: 2025-06-11 08:26 ~ 2025-06-11 08:39
+	Refactoring	:
+	Review
+		- 실제 사용하는 부분의 흐름을 깔끔하게 유지하기 위해 함수를 사용하는게 옳을까?
+		- GPT 리뷰
+			- 함수 호출에도 오버헤드가 있으므로, 짧은 함수는 작성하지 않는게 맞다.
+			  코딩테스트에서는 문제없음.
+			- 초보단계 실수를 줄이고, 흐름 보기 쉽다는 점에서는 옳다.
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <queue>
+
+void CustomPush(std::queue<int>& q) {
+	int input;
+	std::cin >> input;
+	q.push(input);
+}
+
+void CustomPop(std::queue<int>& q) {
+	if (q.empty()) {
+		std::cout << "-1\n";
+	}
+	else {
+		std::cout << q.front() << "\n";
+		q.pop();
+	}
+}
+
+void CustomSize(std::queue<int>& q) {
+	std::cout << q.size() << "\n";
+}
+
+void CustomEmpty(std::queue<int>& q) {
+	if (q.empty()) {
+		std::cout << "1\n";
+	}
+	else {
+		std::cout << "0\n";
+	}
+}
+
+void CustomFront(std::queue<int>& q) {
+	if (q.empty()) {
+		std::cout << "-1\n";
+	}
+	else {
+		std::cout << q.front() << "\n";
+	}
+}
+
+void CustomBack(std::queue<int>& q) {
+	if (q.empty()) {
+		std::cout << "-1\n";
+	}
+	else {
+		std::cout << q.back() << "\n";
+	}
+}
+
+
+int main(void)
+{
+	// Fast I/O
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	// --------[Start]--------
+
+
+	int N; // Num of input commnads
+	std::queue<int> q;
+
+	{ // Input
+		std::cin >> N;
+	}
+
+	std::string cmd = "";
+
+	for (int i = 0; i < N; i++) {
+		std::cin >> cmd;
+
+		if (cmd == "push") CustomPush(q);
+		if (cmd == "pop") CustomPop(q);
+		if (cmd == "size") CustomSize(q);
+		if (cmd == "empty") CustomEmpty(q);
+		if (cmd == "front") CustomFront(q);
+		if (cmd == "back") CustomBack(q);
+	}
+
+
+	// ---------[End]---------
+	return 0;
+}
---
+++ _Core/BOJ11866.cpp
@@ -0,0 +1,61 @@
+﻿/*
+	Site		: BaekJoon Online Judge - https://www.acmicpc.net/
+	Problem		: BOJ 11866
+	URL			: https://www.acmicpc.net/problem/11866
+	Date		: 2025-06-11 08:44 ~ 2025-06-11 08:54
+	Refactoring	:
+	Review
+		- 적절한 자료구조 생각하는데 시간 씀 ... queue 파트에서 들어갔으면서 deque 쓰려했네.
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+
+
+int main(void)
+{
+	// Fast I/O
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	// --------[Start]--------
+
+
+	int N;
+	int K;
+	std::queue<int> q;
+
+	{ // Input
+		std::cin >> N >> K;
+	}
+
+	// TODO: 범위는 항상 이상 ~ 미만으로 하는게 낫지 않을까? 실수 방지
+	for (int i = 1; i <= N; i++) {
+		q.push(i);
+	}
+
+	// TODO: deque에서는 begin()에 ++만 하면 자동으로 계속 도나? 컨테이너 정보
+	// 생각해보니, queue에서 pop하고 push하면 될 일이구나. 중간에서 꺼내려고 deque로 잘못생각함.
+
+	std::vector<int> result;
+	while (!q.empty()) {
+		for (int i = 0; i < K - 1; i++) {
+			q.push(q.front());
+			q.pop();
+		}
+		result.push_back(q.front());
+		q.pop();
+	}
+
+
+	std::cout << "<" << result[0];
+	for (size_t i = 1; i < result.size(); i++) {
+		std::cout << ", " << result[i];
+	}
+	std::cout << ">\n";
+
+
+	// ---------[End]---------
+	return 0;
+}
